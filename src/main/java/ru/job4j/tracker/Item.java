6fc7052a0b0268595b8e525b52ddb49cfc756a60
Petr Arsentev <parsentev@yandex.ru>
2020-09-30 13:23:29 +0300

    6. Date. Отображение даты. [#369960]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,11 @@
 package ru.job4j.tracker;
 
+import java.time.LocalDateTime;
+
 public class Item {
     private int id;
     private String name;
+    private LocalDateTime created = LocalDateTime.now();
 
     public int getId() {
         return id;
@@ -15,6 +18,11 @@ public class Item {
     public String getName() {
         return name;
     }
+
+    public LocalDateTime getTime() {
+        return created;
+    }
+
 
     public void setName(String name) {
         this.name = name;
