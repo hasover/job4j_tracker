b66fb31100c2853c83ea0b2f7f61c1a9b9949c34
hasover <hasover@gmail.com>
2020-10-08 16:04:42 +0300

    0. Map, HashMap [#369886]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,14 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String,String> hashMap = new HashMap<>();
+        hashMap.put("ivan@gmail.com", "Ivan A A");
+        hashMap.put("vlad@gmail.com", "Vlad B B");
+        for (String key: hashMap.keySet()) {
+            System.out.println(key + " " + hashMap.get(key));
+        }
+    }
+}
