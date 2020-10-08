735b9a8c9fcb0e7aa680473c87c58cb320999be9
hasover <hasover@gmail.com>
2020-10-08 19:39:14 +0300

    0. Сортировка [#369876]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Objects;
+
 public class Item {
     private int id;
     private String name;
@@ -13,6 +15,10 @@ public class Item {
     }
 
     public Item() {
+    }
+    public Item(int id, String name) {
+        this.id = id;
+        this.name = name;
     }
 
     public void setId(int id) {
@@ -33,5 +39,18 @@ public class Item {
                 "id=" + id +
                 ", name='" + name + "\'" +
                 '}';
+    }
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return id == item.id &&
+                Objects.equals(name, item.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
     }
 }
---
+++ src/test/java/ru/job4j/tracker/ItemTest.java
@@ -0,0 +1,31 @@
+package ru.job4j.tracker;
+import static org.hamcrest.Matchers.is;
+import org.junit.Test;
+
+import java.util.*;
+
+import static org.junit.Assert.*;
+
+public class ItemTest {
+    @Test
+    public void sortItems() {
+        List<Item> items = Arrays.asList(new Item(5, "A"),
+                                         new Item(2, "B"),
+                                         new Item(3, "C"));
+        Collections.sort(items, new SortItems());
+        assertThat(items, is(Arrays.asList( new Item(2, "B"),
+                                            new Item(3, "C"),
+                                            new Item(5, "A"))));
+    }
+
+    @Test
+    public void sortItemsReverse() {
+        List<Item> items = Arrays.asList(new Item(5, "A"),
+                                         new Item(2, "B"),
+                                         new Item(3, "C"));
+        Collections.sort(items, new SortItemsByNameReverse());
+        assertThat(items, is(Arrays.asList(new Item(3, "C"),
+                                           new Item(2, "B"),
+                                           new Item(5, "A"))));
+    }
+}
