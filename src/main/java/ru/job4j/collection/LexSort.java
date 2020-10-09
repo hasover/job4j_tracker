65d17642ed4924d877a57b08c181ad521494d330
hasover <hasover@gmail.com>
2020-10-09 13:31:46 +0300

    3. Компаратор для строк [#369872]

--- src/main/java/ru/job4j/collection/LexSort.java
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -6,16 +6,8 @@ public class LexSort implements Comparator<String> {
 
     @Override
     public int compare(String left, String right) {
-        return Integer.compare(extractNumber(left), extractNumber(right));
-    }
-
-    private int extractNumber(String string) {
-        int i = 0;
-        String str = "";
-        while (Character.isDigit(string.charAt(i))) {
-            str += string.charAt(i);
-            i++;
-        }
-        return Integer.valueOf(str);
+        String[] leftArr = left.split("\\.");
+        String[] rightArr = right.split("\\.");
+        return Integer.compare(Integer.valueOf(leftArr[0]), Integer.valueOf(rightArr[0]));
     }
 }
