85d51c6acf26d3ceadf630ede78dba33058f2855
hasover <hasover@gmail.com>
2020-10-09 13:09:33 +0300

    3. Компаратор для строк [#369872]

--- src/main/java/ru/job4j/collection/StringCompare.java
+++ src/main/java/ru/job4j/collection/StringCompare.java
@@ -5,9 +5,10 @@ import java.util.Comparator;
 public class StringCompare implements Comparator<String> {
     @Override
     public int compare(String left, String right) {
-        for (int i = 0; i < left.length() && i < right.length(); i++) {
-            if (Character.compare(left.charAt(i), right.charAt(i)) != 0) {
-                return Character.compare(left.charAt(i), right.charAt(i));
+        for (int i = 0; i < Math.min(left.length(), right.length()); i++) {
+            int result = Character.compare(left.charAt(i), right.charAt(i));
+            if (result != 0) {
+                return result;
             }
         }
         return left.length() - right.length();
