cc9f7673f5fdf8c997d4ccfb5cfcee53118cefdf
hasover <hasover@gmail.com>
2020-10-10 11:26:18 +0300

    2. Отсортировать департаменты [#369868]

---
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -0,0 +1,16 @@
+package ru.job4j.collection;
+
+import java.util.Comparator;
+
+public class DepDescComp implements Comparator<String> {
+    @Override
+    public int compare(String o1, String o2) {
+        String[] arr1 = o1.split("/");
+        String[] arr2 = o2.split("/");
+        int result = arr2[0].compareTo(arr1[0]);
+        if (result !=0) {
+            return result;
+        }
+        return o1.compareTo(o2);
+    }
+}
